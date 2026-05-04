0f9705f9d3db34e3c1e268c2a7d1828b11d078e1
AnnStarrySky <anuta7267@mail.ru>
2026-05-04 11:56:39 +0700

    feat: implement search component with localStorage persistence

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { type ChangeEvent } from 'react';
 
-class Search extends React.Component {
+type SearchQuery = {
+  searchValue: string
+}
+
+type Props = Record<string, never>;
+
+class Search extends React.Component<Props, SearchQuery> {
+  state: SearchQuery = {
+    searchValue: localStorage.getItem('searchQuery') || '',
+  }
+
+  handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({searchValue: e.target.value});
+  };
+
+    onSearchClick = () => {
+    const trimmedValue = this.state.searchValue.trim();
+
+    if (trimmedValue){
+      localStorage.setItem('searchQuery', trimmedValue)
+    }
+  };
   render() {
+    const { searchValue } = this.state;
     return (
       <div className="flex gap-2">
         <input
           type="text"
           placeholder="Search..."
           className="border p-2 flex-1"
+          value={searchValue}
+          onChange={this.handleInputChange}
         />
-        <button className="bg-blue-500 text-white px-4">
+        <button 
+        className="bg-blue-500 text-white px-4"
+        onClick={this.onSearchClick}
+        >
           Search
         </button>
       </div>
