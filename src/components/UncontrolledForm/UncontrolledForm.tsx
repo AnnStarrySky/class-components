3d8db8e85a4d78c9bb3e4ae1b48701826fe02ad6
AnnStarrySky <anuta7267@mail.ru>
2026-06-08 12:18:33 +0700

    feat: add Zod validation to uncontrolled form

--- src/components/UncontrolledForm/UncontrolledForm.tsx
+++ src/components/UncontrolledForm/UncontrolledForm.tsx
@@ -1,28 +1,60 @@
+import { useState } from "react";
+import { formSchema } from "../../validation/formSchema";
 import type { FormValues } from "../../validation/formSchema";
 
 type Props = {
-  onSubmit: (data: FormValues ) => void;
+  onSubmit: (data: FormValues) => void;
 };
 
 export function UncontrolledForm({ onSubmit }: Props) {
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     
-        const formData = new FormData(e.currentTarget);
+    const formData = new FormData(e.currentTarget);
     
-        const data: FormValues = {
-            name: String(formData.get("name") ?? ""),
-            email: String(formData.get("email") ?? ""),
-            age: Number(formData.get("age") ?? 0),
-        };
-        onSubmit(data);
+    const rawData = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+      age: Number(formData.get("age")), 
     };
-    return (
-        <form onSubmit={handleSubmit} className="form mt-4">
+
+    const result = formSchema.safeParse(rawData);
+
+    if (!result.success) {
+      const formattedErrors: Record<string, string> = {};
+      result.error.issues.forEach((issue) => {
+        const path = issue.path[0] as string;
+        formattedErrors[path] = issue.message;
+      });
+      
+      setErrors(formattedErrors);
+      return;
+    }
+
+    setErrors({});
+    onSubmit(result.data); 
+  };
+
+  return (
+    <form noValidate onSubmit={handleSubmit} className="form mt-4">
+      <div className="form-group">
         <input className="form-input" name="name" placeholder="Name" required />
+        {errors.name && <p className="error-text">{errors.name}</p>}
+      </div>
+
+      <div className="form-group">
         <input className="form-input" name="email" placeholder="Email" type="email" required />
+        {errors.email && <p className="error-text">{errors.email}</p>}
+      </div>
+
+      <div className="form-group">
         <input className="form-input" name="age" placeholder="Age" type="number" required />
-        <button className="btn" type="submit">Send</button>
-        </form>
-    );
-}    
+        {errors.age && <p className="error-text">{errors.age}</p>}
+      </div>
+
+      <button className="btn" type="submit">Send</button>
+    </form>
+  );
+}
