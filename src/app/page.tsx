c180e30cfbda72a983f1db68199930797df4a2fa
AnnStarrySky <anuta7267@mail.ru>
2026-06-22 18:05:17 +0700

    fix: next.js build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import { Suspense } from 'react'
 import Layout from "../components/Layout/Layout";
 
 export default function Page() {
-  return <Layout />
+  <Suspense fallback={null}>
+      <Layout />
+    </Suspense>
 }
